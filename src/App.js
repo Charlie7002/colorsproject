b63e06f20f78228d1f2b5f3254bbe82f6a3010aa
cha <chaced@orange.fr>
2021-01-29 17:05:43 +0100

    PaletteList Router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,40 @@ import React, { Component } from "react";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
+import { Route, Switch } from "react-router-dom";
+import PaletteList from "./PaletteList";
 
 class App extends Component {
+	findPalette(id) {
+		return seedColors.find((palette) => palette.id === id);
+	}
 	render() {
-		console.log(generatePalette(seedColors[6]));
+		console.log(seedColors);
+		console.log(generatePalette(seedColors[1]));
 		return (
-			<div className="App">
-				<Palette palette={generatePalette(seedColors[6])} />
-			</div>
+			<>
+				<Switch>
+					<Route
+						exact
+						path="/"
+						render={() => <PaletteList palettes={seedColors} />}
+					/>
+					<Route
+						exact
+						path="/palette/:id"
+						render={(routeProps) => (
+							<Palette
+								palette={generatePalette(
+									this.findPalette(routeProps.match.params.id)
+								)}
+							/>
+						)}
+					/>
+				</Switch>
+				{/* <div className="App">
+					<Palette palette={generatePalette(seedColors[6])} />
+				</div> */}
+			</>
 		);
 	}
 }
--- src/Palette.js
+++ src/Palette.js
@@ -3,31 +3,44 @@ import seedColors from "./seedColors";
 import ColorBox from "./ColorBox";
 import Slider from "rc-slider";
 import NavBar from "./NavBar";
-import "./Palette.css";
+import "./Palette.scss";
 
 class Palette extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			level: 500,
+			format: "hex",
 		};
 	}
 	changeLevel = (level) => {
 		this.setState({ level });
 		console.log(this.state.level);
 	};
+
+	changeFormat = (val) => {
+		this.setState({ format: val });
+	};
 	render() {
-		const { colors } = this.props.palette;
-		const { level } = this.state;
+		const { colors, paletteName, emoji } = this.props.palette;
+		const { level, format } = this.state;
 		const colorBoxes = colors[level].map((c) => (
-			<ColorBox background={c.hex} name={c.name} />
+			<ColorBox background={c[format]} name={c.name} key={c.id} />
 		));
 
 		return (
 			<div className="Palette">
-				<NavBar level={level} changeLevel={this.changeLevel} />
+				<NavBar
+					level={level}
+					changeLevel={this.changeLevel}
+					handleChange={this.changeFormat}
+				/>
 
 				<div className="Palette-colors">{colorBoxes}</div>
+				<footer className="Palette-footer">
+					{paletteName}
+					<span className="emoji">{emoji}</span>
+				</footer>
 			</div>
 		);
 	}
